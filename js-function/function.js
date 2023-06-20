ed7666ae467f6f012999502846d7c62fdef63325
holaardipupo1930 <122979897+holaardipupo1930@users.noreply.github.com>
2023-06-20 03:48:20 -0700

    folder
    folder

---
+++ js-function/function.js
@@ -0,0 +1,21 @@
+//display of name
+function sayHi(){
+    const name='Dr Oladipupo Dada';
+    console.log(name);
+}
+
+sayHi();
+
+//average of two number
+function average(a , b){
+    return ( a + b)/2;
+}
+var av1 = average(4 , 2);
+var av2 = average(4 , 6);
+console.log(av1, av2);
+
+//sum of two number
+function sum(a , b){
+    console.log(6 + 8)
+}
+sum();
